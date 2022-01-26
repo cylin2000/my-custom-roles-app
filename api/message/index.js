467dca2c96f6dcd6d1002f67ee8205d194da86ff
Calvin Cai <yuncai@microsoft.com>
2022-01-26 10:32:00 +0800

    update

--- api/message/index.js
+++ api/message/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
     const user = req.body || {};
-
+    //update
     context.res.json({
         id : 'test001',
         text: "Hello from the API",
