c868db9fdff79942e3cfdcee240d99e88c49b355
Calvin Cai <yuncai@microsoft.com>
2023-03-27 18:03:35 +0800

    update roles definition

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -26,7 +26,7 @@ module.exports = async function (context, req) {
     //     }
     // })
     roles.push("admin");
-    roles.push(btoa(JSON.stringify(user)));
+    //roles.push(btoa(JSON.stringify(user)));
 
     context.res.json({
         roles
